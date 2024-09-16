d05a570f556e56df9b584166b09f3ca8a3517c45
Aimee Hong <honglp@bc.edu>
2024-09-16 16:41:46 -0400

    Initial Commit

---
+++ Predict/PredictApp.swift
@@ -0,0 +1,17 @@
+//
+//  PredictApp.swift
+//  Predict
+//
+//  Created by Aimee Hong on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct PredictApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
